ef09368ce64612dadc94adaadae1aa7d014de41f
WinnieCWu <winniechn7@gmail.com>
2022-05-10 15:50:25 -0700

    added photo gallery component

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from "./components/Gallery";
 
 // function App() {
 //   return (
@@ -29,6 +30,7 @@ function App() {
     <div>
       <Nav />
       <main>
+        <Gallery />
         <About />
       </main>
     </div>
