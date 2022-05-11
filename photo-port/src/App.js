9361309023287ffa5013717d0bdfbcde23774c5b
WinnieCWu <winniechn7@gmail.com>
2022-05-10 22:54:14 -0700

    created photolist component and enabled fxnality to display photos

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -15,7 +15,7 @@ function App() {
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
-  
+
   return (
     <div>
       <Nav 
@@ -24,7 +24,7 @@ function App() {
         currentCategory={currentCategory}
       />
       <main>
-        <Gallery />
+        <Gallery currentCategory={currentCategory} />
         <About />
       </main>
     </div>
