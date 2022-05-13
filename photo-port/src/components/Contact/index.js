6944e3b8f9b1f062c7bdf65033bec1a436748e79
WinnieCWu <winniechn7@gmail.com>
2022-05-13 16:51:28 -0700

    enable Contact Form to conditionally render

--- photo-port/src/components/Contact/index.js
+++ photo-port/src/components/Contact/index.js
@@ -1,20 +1,42 @@
 import React, {useState} from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const [errorMessage, setErrorMessage] = useState('');
     const { name, email, message } = formState;
 
-    function handleChange(e) {
-        // setFormState({...formState, [e.target.name]: e.target.value })
+    const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(formState);
-      }
-      
-      console.log(formState);
+        if (!errorMessage) {
+          setFormState({ [e.target.name]: e.target.value });
+          console.log('Form', formState);
+        }
+      };
+
+    function handleChange(e) {
+        //if input is email, then validate and return if is valid
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+              } else {
+                setErrorMessage('');
+              }
+            } else {
+                if (!e.target.value.length) {
+                  setErrorMessage(`${e.target.name} is required.`);
+                } else {
+                  setErrorMessage('');
+                }
+              }
+            };
 
     return (
         <section>
-            <h1> Contact Me </h1>
+            <h1 data-testid="h1tag">Contact me</h1>
             <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
@@ -28,10 +50,16 @@ function ContactForm() {
                     <label htmlFor="message">Message:</label>
                     <textarea name="message" defaultValue={message} onChange={handleChange} rows="5"  />
                 </div>
-                <button type="submit">Submit</button>
-            </form>
-        </section>
-    );
-};
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button data-testid="button" type="submit">Submit</button>
+      </form>
+    </section>
+  );
+}
+
 
 export default ContactForm;
