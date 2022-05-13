cbda0761fd9ef85365aef3b7f358fd603fd0c759
WinnieCWu <winniechn7@gmail.com>
2022-05-13 16:02:36 -0700

    made Contact form a controlled component with synced State

--- photo-port/src/components/Contact/index.js
+++ photo-port/src/components/Contact/index.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function ContactForm() {
-    //JSX
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        // setFormState({...formState, [e.target.name]: e.target.value })
+        e.preventDefault();
+        console.log(formState);
+      }
+      
+      console.log(formState);
+
     return (
         <section>
             <h1> Contact Me </h1>
-            <form id="contact-form">
-                //name input
+            <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
-                    //use htmlFor to avoid error with JS reserved keyword 'for' attribute
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
                 </div>
-                //email input
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" defaultValue={email} name="email" onChange={handleChange} />
                 </div>
-                //message text area
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5"  />
                 </div>
                 <button type="submit">Submit</button>
             </form>
