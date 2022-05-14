e15b6481a978c52cc744fff12ff1f819ef8f0a1b
WinnieCWu <winniechn7@gmail.com>
2022-05-13 17:25:55 -0700

    tested the components and finished designing Contact Form

--- photo-port/src/components/Nav/__tests__/index.test.js
+++ photo-port/src/components/Nav/__tests__/index.test.js
@@ -1,49 +1,55 @@
-import React from 'react';
-import { render, cleanup } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Nav from '..';
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Nav from "..";
 
 afterEach(cleanup);
 
-// const categories = [
-//     {name: 'portraits', description: 'Portraits of my people in life'}
-// ]
-// const mockCurrentCategory = jest.fn();
-// const mockSetCurrentCategory = jest.fn();
+const categories = [
+  { name: "portraits", description: "Portraits of my people in life" },
+];
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 //describe what this suite will be testing
-describe('Nav component', () => {
-    // baseline test
-    it('renders', () => {
-        render(<Nav 
-            // categories={categories}
-            // setCurrentCategory={mockSetCurrentCategory}
-            // currentCategory={mockCurrentCategory}
-        />);
-    });
-    // snapshot test
-    it('matches snapshot', () => {
-        const { asFragment } = render(<Nav />);
-        // assert value comparison
-        expect(asFragment()).toMatchSnapshot();
-      });
-    })
+describe("Nav component", () => {
+  // baseline test
+  it("renders", () => {
+    render(
+      <Nav
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
+      />
+    );
+  });
+  // snapshot test
+  it("matches snapshot", () => {
+    const { asFragment } = render(<Nav />);
+    // assert value comparison
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
 
-    describe('emoji is visible', () => {
-        it('inserts emoji into the h2', () => {
-            //query to return the element containing the emoji
-        const { getByLabelText } = render(<Nav />);
-      
-        expect(getByLabelText('camera')).toHaveTextContent('📸');
-        });
-    })
+describe("emoji is visible", () => {
+  it("inserts emoji into the h2", () => {
+    //query to return the element containing the emoji
+    const { getByLabelText } = render(<Nav />);
 
-    describe('links are visible', () => {
-        it('inserts text into the links', () => {
-          // Arrange
-          const { getByTestId } = render(<Nav />);
-          // Assert
-          expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
-          expect(getByTestId('about')).toHaveTextContent('About me');
-        });
-    })
+    expect(getByLabelText("camera")).toHaveTextContent("📸");
+  });
+});
+
+describe("links are visible", () => {
+  it("inserts text into the links", () => {
+    // Arrange
+    const { getByTestId } = render(<Nav />);
+    // Assert
+    expect(getByTestId("link")).toHaveTextContent("Oh Snap!");
+    expect(getByTestId("about")).toHaveTextContent("About me");
+  });
+});
--- photo-port/src/components/Nav/index.js
+++ photo-port/src/components/Nav/index.js
@@ -19,10 +19,12 @@ function Nav(props) {
       </h2>
       <nav>
         <ul className="flex-row">
-        <li className={`mx-2 ${contactSelected && 'navActive'}`}>
-            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}> About me </a>
+        <li className="mx-2">
+            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}> 
+              About me 
+            </a>
           </li>
-          <li className={"mx-2"}>
+          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
             <span onClick={() => setContactSelected(true)}>Contact</span>
           </li>
           {categories.map((category) => (
